131105e0fa73dca84a8a19d351035f755a2dce45
Isaac Oteng <IsaacOteng@github.com>
2025-07-13 21:32:07 +0000

    Simple Interest Calculator

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -1,6 +1,8 @@
 #Area Calculator
 
-def area_of_rectangle(length, width):
-    area = length * width
-    return print(f"The area of the rectangle is: {area}")
-area_of_rectangle(10, 5)
+length = 10
+width = 5
+
+area = length * width
+
+print(f"The area of the rectangle is: {area}")
