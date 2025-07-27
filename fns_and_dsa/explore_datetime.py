698ed70ba043be8d20e2a8c7e951c0f83d391c75
Isaac Oteng <IsaacOteng@github.com>
2025-07-27 21:52:27 +0000

    Temperature Conversion Tool

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -3,12 +3,13 @@ from datetime import datetime, timedelta
 def display_current_datetime():
     current_date = datetime.now()
     print("Current date and time:", current_date.strftime("%Y-%m-%d %H:%M:%S"))
+    return current_date
 
-def calculate_future_date():
+def calculate_future_date(current_date):
     days = int(input("Enter the number of days to add to the current date: "))
-    future_date = datetime.now() + timedelta(days=days)
+    future_date = current_date + timedelta(days=days)
     print("Future date:", future_date.strftime("%Y-%m-%d"))
 
 if __name__ == "__main__":
-    display_current_datetime()
-    calculate_future_date()
+    current_date = display_current_datetime()
+    calculate_future_date(current_date)
