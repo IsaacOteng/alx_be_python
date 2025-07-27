c6d92ba9059b04c2cdfdc7a26151537aa7168aae
Isaac Oteng <IsaacOteng@github.com>
2025-07-27 21:53:14 +0000

    Temperature Conversion Tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -11,15 +11,14 @@ if __name__ == "__main__":
     temp_input = input("Enter the temperature to convert: ")
     try:
         temperature = float(temp_input)
+        unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
+        if unit == "F":
+            celsius = convert_to_celsius(temperature)
+            print(f"{temperature}°F is {celsius}°C")
+        elif unit == "C":
+            fahrenheit = convert_to_fahrenheit(temperature)
+            print(f"{temperature}°C is {fahrenheit}°F")
+        else:
+            print("Invalid unit. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
     except ValueError:
-        raise ValueError("Invalid temperature. Please enter a numeric value.")
-
-    unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
-    if unit == "F":
-        celsius = convert_to_celsius(temperature)
-        print(f"{temperature}°F is {celsius}°C")
-    elif unit == "C":
-        fahrenheit = convert_to_fahrenheit(temperature)
-        print(f"{temperature}°C is {fahrenheit}°F")
-    else:
-        print("Invalid unit. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
+        print("Invalid temperature. Please enter a numeric value.")
